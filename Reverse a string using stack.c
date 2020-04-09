95b4b43a89d3bde23da0130628fd713fe3c7d5e2
SURYA7766 <63143110+SURYA7766@users.noreply.github.com>
2020-04-09 14:01:59 +0530

    Update Reverse a string using stack.c

--- Reverse a string using stack.c
+++ Reverse a string using stack.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>  
 #include <string.h>  
   
@@ -14,36 +6,32 @@ int top,stack[max];
   
 void push(char x)
 {  
-  
-      // Push(Inserting Element in stack) operation  
-      if(top == max-1)
-      {  
-          printf("stack overflow");  
-      }  else {  
-          stack[++top]=x;  
-      }  
+if(top == max-1)
+{  
+printf("stack overflow");  
+}  
+else 
+{  
+stack[++top]=x;  
+}  
   
 }  
   
 void pop()
 {  
-    // Pop (Removing element from stack)  
-      printf("%c",stack[top--]);  
+printf("%c",stack[top--]);  
 }  
   
   
 main()  
 {  
-   char str[40];
-   printf("Enter the string : \n");
-   scanf("%s",&str);
-   int len = strlen(str);  
-   int i;  
-  
-   for(i=0;i<len;i++)  
-        push(str[i]);  
-  
-   for(i=0;i<len;i++)  
-      pop();
-     
+char str[40];
+printf("Enter the string : \n");
+scanf("%s",&str);
+int len = strlen(str);  
+int i;  
+for(i=0;i<len;i++)  
+push(str[i]);  
+for(i=0;i<len;i++)  
+pop();
 }
